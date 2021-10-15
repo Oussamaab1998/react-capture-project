959abd857a76ac438d0b00726f65e1e0ee5feb2c
Oussama Abdallah <you@example.com>
2021-10-15 16:48:27 +0200

    2 - About Us

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+
+// Import Pages
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi Oussama Abdallah</h1>
+      <AboutUs />
     </div>
   );
 }
