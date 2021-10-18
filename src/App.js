76e727a1552aea75735195a078416978071afd72
Oussama Abdallah <you@example.com>
2021-10-18 21:00:43 +0200

    7 - Project Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 // Import Pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import Contact from './pages/Contact';
+// Import Global Nav
+import Nav from './components/Nav';
 //Globla Style
 import GlobalStyle from './components/GlobalStyle';
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs  />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
